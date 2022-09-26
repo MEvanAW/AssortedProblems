bfdeeef31b98a62e68ac044ed1b35a4ff0fa8b4d
MEvanAW <m.evan.aw@gmail.com>
2022-09-26 08:37:58 +0700

    Add project files.

---
+++ AssortedProblemsConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using FizzBuzz;
+
+for (int i = 1; i <= 100; i++)
+{
+    string s = Identifier.TypeToString(Identifier.WhatType(i));
+    if (s != "Null")
+    {
+        Console.WriteLine(s);
+    } else
+    {
+        Console.WriteLine(i);
+    }
+}
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FizzBuzz
+{
+    public static class Identifier
+    {
+        public enum Type: byte
+        {
+            Null,
+            Neither,
+            Fizz,
+            Buzz,
+            FizzBuzz
+        }
+        public static Type WhatType(int num)
+        {
+            if (num % 3 == 0)
+            {
+                if (num % 5 == 0)
+                {
+                    return Type.FizzBuzz;
+                }
+                return Type.Fizz;
+            }
+            if (num % 5 == 0)
+            {
+                return Type.Buzz;
+            }
+            return Type.Neither;
+        }
+        public static string TypeToString(Type type)
+        {
+            switch (type)
+            {
+                case Type.Fizz:
+                    return "Fizz";
+                case Type.Buzz:
+                    return "Buzz";
+                case Type.FizzBuzz:
+                    return "FizzBuzz";
+                default:
+                    return "Null";
+            }
+        }
+    }
+}
