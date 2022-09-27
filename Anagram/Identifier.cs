13e6e0ac12fffaa504374cc6b1b5959aa10057ee
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-09-27 14:42:04 +0700

    Add Anagram library and unit test.

---
+++ Anagram/Identifier.cs
@@ -0,0 +1,10 @@
+﻿namespace Anagram
+{
+    public static class Identifier
+    {
+        public static bool IsAnagram(string a, string b)
+        {
+            return a == b;
+        }
+    }
+}
