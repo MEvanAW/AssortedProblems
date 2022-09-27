e428915c28ec8829a5c6527719fcb088303556d1
MEvanAW <m.evan.aw@gmail.com>
2022-09-27 11:15:25 +0700

    Implements Palindrome Unit.

--- Palindrome/Identifier.cs
+++ Palindrome/Identifier.cs
@@ -4,6 +4,13 @@
     {
         public static bool IsPalindrome(string s)
         {
+            int len = s.Length;
+            int mid = len/2;
+            for (int i = 0; i < mid; i++)
+            {
+                if (s[i] != s[len - 1 - i])
+                    return false;
+            }
             return true;
         }
     }
