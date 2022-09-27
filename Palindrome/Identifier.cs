a02fe37a3cb5b153e668d8c18965d27bf4aff994
M. Evan Anindya W <m.evan.aw@gmail.com>
2022-09-27 12:35:41 +0700

    Adds XML documentation for Palindrome library.

--- Palindrome/Identifier.cs
+++ Palindrome/Identifier.cs
@@ -1,7 +1,15 @@
 ﻿namespace Palindrome
 {
+    /// <summary>
+    /// Class to identify palindrome.
+    /// </summary>
     public static class Identifier
     {
+        /// <summary>
+        /// Determine whether the string is a palindrome.
+        /// </summary>
+        /// <param name="s">The string to be determined.</param>
+        /// <returns>Whether the string a palindrome or not.</returns>
         public static bool IsPalindrome(string s)
         {
             int len = s.Length;
