f8b90911b5adc70400b126db1148ea65f6882a2c
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-05-21 20:19:15 +0700

    Add Codeland.

---
+++ AssortedProblemsTests/CodelandTest.cs
@@ -0,0 +1,25 @@
+﻿using Codeland;
+
+namespace AssortedProblemsTests
+{
+    public class CodelandTest
+    {
+        [Fact]
+        public void InvalidUsernameDueToFirstRuleTest()
+        {
+            Assert.Equal(UserService.FALSE_STRING, UserService.CodelandUsernameValidation("aa_"));
+        }
+
+        [Fact]
+        public void InvalidUsernameDueToFourthRuleTest()
+        {
+            Assert.Equal(UserService.FALSE_STRING, UserService.CodelandUsernameValidation("aa_"));
+        }
+
+        [Fact]
+        public void ValidUsernameTest()
+        {
+            Assert.Equal(UserService.TRUE_STRING, UserService.CodelandUsernameValidation("u__hello_world123"));
+        }
+    }
+}
---
+++ Codeland/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace Codeland
+{
+    /// <summary>
+    /// Class for Codeland https://coderbyte.com/editor/Codeland%20Username%20Validation:Csharp
+    /// </summary>
+    public static class UserService
+    {
+        public const string FALSE_STRING = "false";
+        public const string TRUE_STRING = "true";
+        
+        /// <summary>
+        /// Validate username according to the following rules:
+        /// 1. The username is between 4 and 25 characters.
+        /// 2. It must start with a letter.
+        /// 3. It can only contain letters, numbers, and the underscore character.
+        /// 4. It cannot end with an underscore character.
+        /// </summary>
+        /// <param name="username">User name to be validated.</param>
+        /// <returns>The string true if the user name is valid, otherwise the string false.</returns>
+        public static string CodelandUsernameValidation(string username)
+        {
+            return FALSE_STRING;
+        }
+    }
+}
