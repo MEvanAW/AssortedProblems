fc57fe98e1d485081ef5cd03be5d3fa4dd01c1b3
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-05-23 10:58:08 +0700

    Add FindIntersection method and a test case.

--- AssortedProblemsTests/IntersectionUnitTest.cs
+++ AssortedProblemsTests/IntersectionUnitTest.cs
@@ -5,9 +5,12 @@ namespace AssortedProblemsTests
     public class IntersectionUnitTest
     {
         [Fact]
-        public void Test()
+        public void IntersectionExistTest()
         {
-            // Assert here
+            var strArr = new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" };
+            Assert.Equal("1,4,13", IntersectionFinder.FindIntersection(strArr));
+            strArr = new string[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" };
+            Assert.Equal("1,9,10", IntersectionFinder.FindIntersection(strArr));
         }
     }
 }
--- Intersection/IntersectionFinder.cs
+++ Intersection/IntersectionFinder.cs
@@ -5,6 +5,15 @@
     /// </summary>
     public static class IntersectionFinder
     {
-        // Implement the class here
+        /// <summary>
+        /// Finding intersection within an array of number strings.
+        /// </summary>
+        /// <param name="strArr">Array of number strings</param>
+        /// <returns>A comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.</returns>
+        public static string FindIntersection(string[] strArr)
+        {
+            // TODO: Implement the method.
+            return strArr[0];
+        }
     }
 }
