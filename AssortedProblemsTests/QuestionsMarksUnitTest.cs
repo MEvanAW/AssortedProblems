7f86cb9f79c81c4d5ea464f5bdb8fea0ef29fa49
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-05-28 11:37:02 +0700

    Add QuestionsMarks.

---
+++ AssortedProblemsTests/QuestionsMarksUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using QuestionsMarks;
+
+namespace AssortedProblemsTests
+{
+    public class QuestionsMarksUnitTest
+    {
+        [Fact]
+        public void FalseTest()
+        {
+            Assert.Equal(PatternChecker.FALSE_STRING, PatternChecker.QuestionsMarks("aa6?9"));
+        }
+
+        [Fact]
+        public void TrueTest()
+        {
+            Assert.Equal(PatternChecker.TRUE_STRING, PatternChecker.QuestionsMarks("acc?7??sss?3rr1??????5"));
+        }
+    }
+}
---
+++ QuestionsMarks/PatternChecker.cs
@@ -0,0 +1,14 @@
+﻿namespace QuestionsMarks
+{
+    public static class PatternChecker
+    {
+        public const string FALSE_STRING = "false";
+        public const string TRUE_STRING = "true";
+
+        public static string QuestionsMarks(string str)
+        {
+            // code goes here  
+            return str;
+        }
+    }
+}
