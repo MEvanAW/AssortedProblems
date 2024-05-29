a4e1f39ebc323681af7ea4d7853485dbe6d79c9f
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-05-29 10:42:55 +0700

    Add CoderbyteConsoleApp.

---
+++ CoderbyteConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://coderbyte.com/information/First%20Factorial
+using System;
+using System.Linq;
+
+class MainClass
+{
+
+    public static int FirstFactorial(int num)
+    {
+        int result = Enumerable.Range(1, num).Aggregate((x, y) =>  x * y );
+        return result;
+
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(int.Parse(Console.ReadLine())));
+
+    }
+
+}
