20ff199efff9a4d61a8447bf2e93c05db161620c
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-06-06 09:32:39 +0700

    Edit CoderbyteConsoleApp to be Diagonal Difference challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem
+﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
@@ -17,27 +17,21 @@ class Result
 {
 
     /*
-     * Complete the 'lonelyinteger' function below.
+     * Complete the 'diagonalDifference' function below.
      *
      * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY a as parameter.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
      */
 
-    public static int lonelyinteger(List<int> a)
+    public static int diagonalDifference(List<List<int>> arr)
     {
-        var countDictionary = new Dictionary<int, int>();
-        foreach (int i in a)
+        int len = arr.Count();
+        int difference = 0;
+        for (int i = 0; i < len; i++)
         {
-            if (countDictionary.ContainsKey(i))
-            {
-                ++countDictionary[i];
-            }
-            else
-            {
-                countDictionary[i] = 1;
-            }
+            difference += arr[i][i] - arr[i][len-i-1];
         }
-        return countDictionary.Where(i => i.Value == 1).Single().Key;
+        return difference >= 0 ? difference : difference * -1;
     }
 
 }
@@ -48,9 +42,14 @@ class Solution
     {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+        List<List<int>> arr = new List<List<int>>();
 
-        int result = Result.lonelyinteger(a);
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
 
         Console.WriteLine(result);
     }
