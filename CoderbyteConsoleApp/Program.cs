ae3111cdda6661408ab7926f10cda918484009a6
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-06-22 14:12:55 +0700

    Edit CoderbyteConsoleApp to be Grid Challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-caesar-cipher-1/problem
+﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-grid-challenge/problem
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
@@ -17,44 +17,35 @@ class Result
 {
 
     /*
-     * Complete the 'caesarCipher' function below.
+     * Complete the 'gridChallenge' function below.
      *
      * The function is expected to return a STRING.
-     * The function accepts following parameters:
-     *  1. STRING s
-     *  2. INTEGER k
+     * The function accepts STRING_ARRAY grid as parameter.
      */
 
-    public static string caesarCipher(string s, int k)
+    public static string gridChallenge(List<string> grid)
     {
-        int shift = k % 26;
-        string cipherText = string.Empty;
-        foreach (char c in s)
+        int rowCount = grid.Count();
+        for (int row = 0; row < rowCount; ++row)
         {
-            if (char.IsLower(c))
+            var charList = grid[row].ToList();
+            charList.Sort();
+            grid[row] = string.Concat(charList);
+        }
+        int colCount = grid[0].Count();
+        for (int col = 0; col < colCount; ++col)
+        {
+            char previous = 'a';
+            for (int row = 0; row < rowCount; ++row)
             {
-                var cipherC = c + shift;
-                if (cipherC > 'z')
+                if (grid[row][col] < previous)
                 {
-                    cipherC -= 26;
+                    return "NO";
                 }
-                cipherText += Convert.ToChar(cipherC);
-            }
-            else if (char.IsUpper(c))
-            {
-                var cipherC = c + shift;
-                if (cipherC > 'Z')
-                {
-                    cipherC -= 26;
-                }
-                cipherText += Convert.ToChar(cipherC);
-            }
-            else
-            {
-                cipherText += c;
+                previous = grid[row][col];
             }
         }
-        return cipherText;
+        return "YES";
     }
 
 }
@@ -65,15 +56,24 @@ class Solution
     {
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
 
-        string s = Console.ReadLine();
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        int k = Convert.ToInt32(Console.ReadLine().Trim());
+            List<string> grid = new List<string>();
 
-        string result = Result.caesarCipher(s, k);
+            for (int i = 0; i < n; i++)
+            {
+                string gridItem = Console.ReadLine();
+                grid.Add(gridItem);
+            }
 
-        textWriter.WriteLine(result);
+            string result = Result.gridChallenge(grid);
+
+            textWriter.WriteLine(result);
+        }
 
         textWriter.Flush();
         textWriter.Close();
