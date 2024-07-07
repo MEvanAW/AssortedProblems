b9e7a6dbb6d5bdf82e4cf13735497933b0cc2238
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-07-07 12:53:31 +0700

    Edit CoderbyteConsoleApp to be Queue challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,161 +1,35 @@
-﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-merge-two-sorted-linked-lists/problem
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
+﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-queue-using-two-stacks/problem
 using System;
-
+using System.Collections.Generic;
+using System.IO;
 class Solution
 {
-
-    class SinglyLinkedListNode
+    static void Main(String[] args)
     {
-        public int data;
-        public SinglyLinkedListNode next;
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        const string ENQUEUE = "1";
+        const string DEQUEUE = "2";
+        const string PRINT = "3";
 
-        public SinglyLinkedListNode(int nodeData)
+        int q = Convert.ToInt32(Console.ReadLine());
+        var queue = new Queue<string>();
+
+        for (int testsItr = 0; testsItr < q; ++testsItr)
         {
-            this.data = nodeData;
-            this.next = null;
-        }
-    }
-
-    class SinglyLinkedList
-    {
-        public SinglyLinkedListNode head;
-        public SinglyLinkedListNode tail;
-
-        public SinglyLinkedList()
-        {
-            this.head = null;
-            this.tail = null;
-        }
-
-        public void InsertNode(int nodeData)
-        {
-            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
-
-            if (this.head == null)
+            string line = Console.ReadLine()!;
+            string[] query = line.Split(' ');
+            if (query[0] == ENQUEUE)
             {
-                this.head = node;
+                queue.Enqueue(query[1]);
             }
-            else
+            else if (query[0] == DEQUEUE)
             {
-                this.tail.next = node;
+                queue.Dequeue();
             }
-
-            this.tail = node;
-        }
-    }
-
-    static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep)
-    {
-        while (node != null)
-        {
-            Console.Write(node.data);
-
-            node = node.next;
-
-            if (node != null)
+            else if (query[0] == PRINT)
             {
-                Console.Write(sep);
+                Console.WriteLine(queue.Peek());
             }
-        }
-    }
-
-    // Complete the mergeLists function below.
-
-    /*
-     * For your reference:
-     *
-     * SinglyLinkedListNode {
-     *     int data;
-     *     SinglyLinkedListNode next;
-     * }
-     *
-     */
-    static SinglyLinkedListNode mergeLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
-    {
-        if (head1 == null)
-        {
-            return head2;
-        }
-        if (head2 == null)
-        {
-            return head1;
-        }
-        var pointerA = head1;
-        var pointerB = head2;
-        SinglyLinkedListNode newHead;
-        if (head1.data <= head2.data)
-        {
-            newHead = head1;
-            pointerA = pointerA.next;
-        }
-        else
-        {
-            newHead = head2;
-            pointerB = pointerB.next;
-        }
-        var newPointer = newHead;
-        while (pointerA != null || pointerB != null)
-        {
-            if ((pointerA?.data ?? int.MaxValue) <= (pointerB?.data ?? int.MaxValue))
-            {
-                var temp = pointerA!;
-                pointerA = pointerA!.next;
-                newPointer.next = temp;
-                newPointer = newPointer.next;
-            }
-            else
-            {
-                var temp = pointerB!;
-                pointerB = pointerB!.next;
-                newPointer.next = temp;
-                newPointer = newPointer.next;
-            }
-        }
-        return newHead;
-    }
-
-    static void Main(string[] args)
-    {
-        int tests = Convert.ToInt32(Console.ReadLine());
-
-        for (int testsItr = 0; testsItr < tests; testsItr++)
-        {
-            SinglyLinkedList llist1 = new SinglyLinkedList();
-
-            int llist1Count = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < llist1Count; i++)
-            {
-                int llist1Item = Convert.ToInt32(Console.ReadLine());
-                llist1.InsertNode(llist1Item);
-            }
-
-            SinglyLinkedList llist2 = new SinglyLinkedList();
-
-            int llist2Count = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < llist2Count; i++)
-            {
-                int llist2Item = Convert.ToInt32(Console.ReadLine());
-                llist2.InsertNode(llist2Item);
-            }
-
-            SinglyLinkedListNode llist3 = mergeLists(llist1.head, llist2.head);
-
-            PrintSinglyLinkedList(llist3, " ");
-            Console.WriteLine();
         }
     }
 }
