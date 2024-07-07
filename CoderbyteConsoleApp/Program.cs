beafb9bbddfeae661ca75ca364f2e290e0e0d059
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-07-07 14:08:10 +0700

    Edit CoderbyteConsoleApp to be Balanced Brackets challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,35 +1,91 @@
-﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-queue-using-two-stacks/problem
-using System;
+﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-balanced-brackets/problem
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    const string NO = "NO";
+    const string YES = "YES";
+
+    /*
+     * Complete the 'isBalanced' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static string isBalanced(string s)
+    {
+        var opening = new char[]
+        {
+            '(',
+            '[',
+            '{'
+        };
+        var stack = new Stack<char>(s.Count()/2);
+        foreach (char c in s)
+        {
+            if (opening.Contains(c))
+            {
+                stack.Push(c);
+            }
+            else if (stack.Count <= 0)
+            {
+                return NO;
+            }
+            else
+            {
+                var peek = stack.Peek();
+                if (peek == '(' && c != ')')
+                {
+                    return NO;
+                }
+                else if (peek == '[' && c != ']')
+                {
+                    return NO;
+                }
+                else if (peek == '{' && c != '}')
+                {
+                    return NO;
+                }
+                stack.Pop();
+            }
+        }
+        if (stack.Count > 0)
+        {
+            return NO;
+        }
+        return YES;
+    }
+}
+
 class Solution
 {
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        const string ENQUEUE = "1";
-        const string DEQUEUE = "2";
-        const string PRINT = "3";
-
-        int q = Convert.ToInt32(Console.ReadLine());
-        var queue = new Queue<string>();
-
-        for (int testsItr = 0; testsItr < q; ++testsItr)
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+        List<string> toBeWritten = new();
+        for (int tItr = 0; tItr < t; tItr++)
         {
-            string line = Console.ReadLine()!;
-            string[] query = line.Split(' ');
-            if (query[0] == ENQUEUE)
-            {
-                queue.Enqueue(query[1]);
-            }
-            else if (query[0] == DEQUEUE)
-            {
-                queue.Dequeue();
-            }
-            else if (query[0] == PRINT)
-            {
-                Console.WriteLine(queue.Peek());
-            }
+            string s = Console.ReadLine();
+
+            string result = Result.isBalanced(s);
+
+            toBeWritten.Add(result);
+        }
+        foreach (var s in toBeWritten)
+        {
+            Console.WriteLine(s);
         }
     }
 }
