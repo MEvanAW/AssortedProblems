2bda9dcf9efb7bf3503daa1e5b82ceefde4ad249
M. Evan Anindya W <m.evan.aw@gmail.com>
2024-07-29 13:05:17 +0700

    Edit CoderbyteConsoleApp to be Lego Blocks challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,47 +1,141 @@
-﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-simple-text-editor/problem
-using System;
+﻿// https://www.hackerrank.com/challenges/one-week-preparation-kit-lego-blocks/problem
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    const int M = 1000000007;
+    // Example cases:
+    // m = 1: {1}
+    // m = 2: {1,1}, {2}
+    // m = 3: {1,1,1}, {1,2}, {2,1}, {3}
+    // m = 4: {1,1,1,1}, {1,1,2}, {1,2,1}, {2,1,1}, {1,3}, {3,1}, {2,2}, {4}
+    // m = 5: {1,1,1,1,1}, {1,1,1,2}, {1,1,2,1}, {1,2,1,1}, {2,1,1,1}, {1,2,2}, {2,1,2}, {2,2,1}, {1,1,3}, {1,3,1}, {3,1,1}, {2,3}, {3,2}, {1,4}, {4,1}
+    static private List<decimal> horizontalPermutations = new List<decimal> { 1, 2, 4, 8, 15, 29, 56, 108, 208, 401, 773, 1490, 2872, 5536, 10671, 20569, 39648, 76424, 147312 };
+    static private Dictionary<int, List<decimal>> nToWallPermutationsDict = new();
+    static private Dictionary<int, List<decimal>> nToGoodPermutationsDict = new();
+
+    /*
+     * Complete the 'legoBlocks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+    public static int legoBlocks(int n, int m)
+    {
+        if (m > horizontalPermutations.Count)
+        {
+            // Calculate number of permutations for a line.
+            // Each iteration, include 1 width block after all the i-1 width ways plus 2-width block after all the i-2 width ways, etc. up to the 4 width block
+            decimal aggregate = horizontalPermutations[horizontalPermutations.Count - 1];
+            for (int i = horizontalPermutations.Count; i < m; ++i)
+            {
+                if (i > 101)
+                {
+                    aggregate = (aggregate % M + Mod(horizontalPermutations[i - 1] % M - horizontalPermutations[i - 5] % M)) % M;
+                }
+                else
+                {
+                    aggregate += (horizontalPermutations[i - 1] - horizontalPermutations[i - 5]);
+                }
+                horizontalPermutations.Add(aggregate);
+            }
+        }
+        var wallPermutations = new List<decimal>();
+        int mIndex = 0;
+        bool nExist = nToWallPermutationsDict.ContainsKey(n);
+        if (nExist)
+        {
+            wallPermutations = nToWallPermutationsDict[n];
+            mIndex = wallPermutations.Count;
+        }
+        for (; mIndex < m; ++mIndex)
+        {
+            wallPermutations.Add(ModulatedPower(horizontalPermutations[mIndex], n));
+        }
+        if (!nExist) {
+            nToWallPermutationsDict[n] = wallPermutations;
+        }
+        // There is only 1 way to make a width-1 good wall
+        var goodPermutations = new List<decimal> { 1 };
+        mIndex = 1;
+        nExist = nToGoodPermutationsDict.ContainsKey(n);
+        if (nExist)
+        {
+            goodPermutations = nToGoodPermutationsDict[n];
+            mIndex = goodPermutations.Count;
+        }
+        // Any good wall permutation of width < m is essentially one of the invalid left portions of a bad wall, so we subtract those out, multiplied by the permutations
+        for (;  mIndex < m; ++mIndex)
+        {
+            decimal goodPermutation = wallPermutations[mIndex];
+            for (int j = 0; j < mIndex; ++j)
+            {
+                goodPermutation = Mod(goodPermutation % M - ((goodPermutations[j] % M) * (wallPermutations[mIndex - j - 1] % M) % M) % M);
+            }
+            goodPermutations.Add(goodPermutation);
+        }
+        if (!nExist)
+        {
+            nToGoodPermutationsDict[n] = goodPermutations;
+        }
+        return (int) goodPermutations[m - 1];
+    }
+
+    private static decimal ModulatedPower(decimal x, int exp)
+    {
+        decimal result = 1;
+        while (exp > 0)
+        {
+            if ((exp & 1) == 1)
+            {
+                result %= M;
+                result = (result * x);
+            }
+            exp >>= 1;
+            x %= M;
+            x = (x * x);
+        }
+        return result;
+    }
+
+    private static decimal Mod(decimal x)
+    {
+        decimal mod = x % M;
+        return mod >= 0 ? mod : mod + M;
+    }
+}
+
 class Solution
 {
-    const string APPEND = "1";
-    const string DELETE = "2";
-    const string PRINT = "3";
-    //const string UNDO = "4";
-
-    static void Main(String[] args)
+    public static void Main(string[] args)
     {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
-        int Q = int.Parse(Console.ReadLine());
-        var ops = new string[Q];
-        for (int i = 0; i < Q; ++i)
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
         {
-            ops[i] = Console.ReadLine();
-        }
-        string S = string.Empty;
-        var stack = new Stack<string>();
-        stack.Push(S);
-        foreach (string op in ops)
-        {
-            var opArr = op.Split(' ');
-            if (opArr[0] == APPEND)
-            {
-                stack.Push(S);
-                S += opArr[1];
-            }
-            else if (opArr[0] == DELETE)
-            {
-                stack.Push(S);
-                S = S.Remove(S.Length - int.Parse(opArr[1]));
-            }
-            else if (opArr[0] == PRINT)
-            {
-                Console.WriteLine(S[int.Parse(opArr[1]) - 1]);
-            }
-            else
-            {
-                S = stack.Pop();
-            }
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = Result.legoBlocks(n, m);
+
+            Console.WriteLine(result);
         }
     }
 }
