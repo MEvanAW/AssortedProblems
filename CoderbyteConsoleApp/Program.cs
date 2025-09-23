8e6aed1896f137ca0e71171f498edacb91892a0c
M. Evan Anindya W <m.evan.aw@gmail.com>
2025-09-23 11:17:00 +0700

    Edit CoderbyteConsoleApp to be Lookup with Binary Search challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/check-non-identical-string-rotation/problem?isFullScreen=true
+﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/lookup-with-binary-search/problem?isFullScreen=true
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
@@ -17,21 +17,51 @@ class Result
 {
 
     /*
-     * Complete the 'isNonTrivialRotation' function below.
+     * Complete the 'binarySearch' function below.
      *
-     * The function is expected to return a BOOLEAN.
+     * The function is expected to return an INTEGER.
      * The function accepts following parameters:
-     *  1. STRING s1
-     *  2. STRING s2
+     *  1. INTEGER_ARRAY nums
+     *  2. INTEGER target
      */
 
-    public static bool isNonTrivialRotation(string s1, string s2)
+    public static int binarySearch(List<int> nums, int target)
     {
-        if (s1 == s2)
+        if (nums.Count == 1) return nums[0] == target ? 0 : -1;
+        var lowerBound = 0;
+        var upperBound = nums.Count - 1;
+        var checkingIndex = upperBound / 2;
+        while (lowerBound < upperBound)
         {
-            return false;
+            int range;
+            if (nums[checkingIndex] == target)
+            {
+                return checkingIndex;
+            }
+            if (nums[checkingIndex] > target)
+            {
+                upperBound = checkingIndex;
+                range = upperBound - lowerBound;
+                if (range == 1)
+                {
+                    --checkingIndex;
+                }
+            }
+            else
+            {
+                lowerBound = checkingIndex;
+                range = upperBound - lowerBound;
+                if (range == 1)
+                {
+                    ++checkingIndex;
+                }
+            }
+            if (range > 1)
+            {
+                checkingIndex = (upperBound + lowerBound) / 2;
+            }
         }
-        return (s1 + s1).Contains(s2);
+        return -1;
     }
 }
 
@@ -39,12 +69,20 @@ class Solution
 {
     public static void Main(string[] args)
     {
-        string s1 = Console.ReadLine();
+        int numsCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-        string s2 = Console.ReadLine();
+        List<int> nums = new List<int>();
 
-        bool result = Result.isNonTrivialRotation(s1, s2);
+        for (int i = 0; i < numsCount; i++)
+        {
+            int numsItem = Convert.ToInt32(Console.ReadLine().Trim());
+            nums.Add(numsItem);
+        }
 
-        Console.WriteLine((result ? 1 : 0));
+        int target = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.binarySearch(nums, target);
+
+        Console.WriteLine(result);
     }
 }
