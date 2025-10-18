aeb6f03b5d97647fa414c242b4f6c284ac1d485b
M. Evan Anindya W <m.evan.aw@gmail.com>
2025-10-18 13:17:59 +0700

    Edit CoderbyteConsoleApp to be Place N Cameras Without Conflict on Blocked Grid challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/lexicographical-letter-combinations-phone-digits/problem?isFullScreen=true
+﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/place-n-cameras-no-conflict-blocked-grid/problem?isFullScreen=true
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
@@ -15,57 +15,70 @@ using System;
 
 class Result
 {
-    private static readonly Dictionary<char, char[]> _mapping = new()
-    {
-        { '0' , new char[] { '0' } },
-        { '1' , new char[] { '1' } },
-        { '2' , new char[] { 'a', 'b', 'c' } },
-        { '3' , new char[] { 'd', 'e', 'f' } },
-        { '4' , new char[] { 'g', 'h', 'i' } },
-        { '5' , new char[] { 'j', 'k', 'l' } },
-        { '6' , new char[] { 'm', 'n', 'o' } },
-        { '7' , new char[] { 'p', 'q', 'r', 's' } },
-        { '8' , new char[] { 't', 'u', 'v' } },
-        { '9' , new char[] { 'w', 'x', 'y', 'z' } }
-    };
 
     /*
-     * Complete the 'minTasksToCancelForNoConflict' function below.
+     * Complete the 'canPlaceSecurityCameras' function below.
      *
-     * The function is expected to return a STRING_ARRAY.
-     * The function accepts STRING digits as parameter.
+     * The function is expected to return a BOOLEAN.
+     * The function accepts following parameters:
+     *  1. INTEGER N
+     *  2. 2D_INTEGER_ARRAY grid
      */
 
-    public static List<string> minTasksToCancelForNoConflict(string digits)
+    public static bool canPlaceSecurityCameras(int N, List<List<int>> grid)
     {
-        var list = new List<string>();
-        map(digits, list, 0, string.Empty);
-        return list;
+        var usedColumns = new HashSet<int>();
+        var usedDiagonals = new HashSet<int>();
+        var usedAntiDiagonals = new HashSet<int>();
+
+        return recursivePlaceCameras(grid, usedColumns, usedDiagonals, usedAntiDiagonals, 0);
     }
 
-    private static void map(string digits, List<string> list, int index, string combination)
+    private static bool recursivePlaceCameras(List<List<int>> grid, HashSet<int> usedColumns, HashSet<int> usedDiagonals, HashSet<int> usedAntiDiagonals, int row)
     {
-        if (index >= digits.Length)
+        if (row == grid.Count)
         {
-            list.Add(combination);
-            return;
+            return true;
         }
-        foreach (var c in _mapping[digits[index]])
+        for (int col = 0; col < grid.Count; ++col)
         {
-            map(digits, list, index + 1, combination + c);
+            if (grid[row][col] == 1 || usedColumns.Contains(col) || usedDiagonals.Contains(row + col) || usedAntiDiagonals.Contains(row - col))
+            {
+                continue;
+            }
+            usedColumns.Add(col);
+            usedDiagonals.Add(row + col);
+            usedAntiDiagonals.Add(row - col);
+            if (recursivePlaceCameras(grid, usedColumns, usedDiagonals, usedAntiDiagonals, row + 1))
+            {
+                return true;
+            }
+            usedColumns.Remove(col);
+            usedDiagonals.Remove(row + col);
+            usedAntiDiagonals.Remove(row - col);
         }
+        return false;
     }
-
 }
 
 class Solution
 {
     public static void Main(string[] args)
     {
-        string digits = Console.ReadLine();
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
 
-        List<string> result = Result.minTasksToCancelForNoConflict(digits);
+        int gridRows = Convert.ToInt32(Console.ReadLine().Trim());
+        int gridColumns = Convert.ToInt32(Console.ReadLine().Trim());
 
-        Console.WriteLine(String.Join("\n", result));
+        List<List<int>> grid = new List<List<int>>();
+
+        for (int i = 0; i < gridRows; i++)
+        {
+            grid.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(gridTemp => Convert.ToInt32(gridTemp)).ToList());
+        }
+
+        bool result = Result.canPlaceSecurityCameras(N, grid);
+
+        Console.WriteLine((result ? 1 : 0));
     }
 }
