0c27de20b42708ec8a10bb3c7756af47ee0ce0e9
M. Evan Anindya W <m.evan.aw@gmail.com>
2025-10-20 11:01:51 +0700

    Edit CoderbyteConsoleApp to be Ways to Fill Slots with Single or Double Coverage challenge.

--- CoderbyteConsoleApp/Program.cs
+++ CoderbyteConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/custom-fibonacci-sequence/problem?isFullScreen=true
+﻿// https://www.hackerrank.com/contests/software-engineer-prep-kit/challenges/ways-to-fill-slots-single-double-coverage/problem?isFullScreen=true
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
@@ -12,33 +12,47 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+using System.Numerics;
 
 class Result
 {
-    private static readonly long[] _fibonacci = new long[] { 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
+    private static readonly int[] _fibonacci = new int[] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 };
 
     /*
-     * Complete the 'getAutoSaveInterval' function below.
+     * Complete the 'countInstallationSequences' function below.
      *
-     * The function is expected to return a LONG_INTEGER.
+     * The function is expected to return a STRING.
      * The function accepts INTEGER n as parameter.
      */
 
-    public static long getAutoSaveInterval(int n)
+    public static string countInstallationSequences(int n)
     {
         if (n < _fibonacci.Length)
         {
-            return _fibonacci[n];
+            return _fibonacci[n].ToString();
         }
-        long operand = _fibonacci[_fibonacci.Length - 2];
-        long sum = _fibonacci[_fibonacci.Length - 1];
-        for (int i = _fibonacci.Length; i <= n; ++i)
+        decimal operand = _fibonacci[_fibonacci.Length - 2];
+        decimal sum = _fibonacci[_fibonacci.Length - 1];
+        int i = _fibonacci.Length;
+        for (; i <= Math.Min(n, 138); ++i)
         {
-            long temp = sum;
+            decimal temp = sum;
             sum += operand;
             operand = temp;
         }
-        return sum;
+        if (i == n + 1)
+        {
+            return sum.ToString();
+        }
+        BigInteger bigOperand = new(operand);
+        BigInteger bigSum = new(sum);
+        for (; i <= n; ++i)
+        {
+            BigInteger temp = bigSum;
+            bigSum += bigOperand;
+            bigOperand = temp;
+        }
+        return bigSum.ToString();
     }
 }
 
@@ -48,7 +62,7 @@ class Solution
     {
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-        long result = Result.getAutoSaveInterval(n);
+        string result = Result.countInstallationSequences(n);
 
         Console.WriteLine(result);
     }
